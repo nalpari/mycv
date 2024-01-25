0b61d363e3e3949751f1b1020f1105c842326528
sangwook.yoo <sangwook.yoo@cj.net>
2024-01-25 10:04:29 +0900

    initial commit

---
+++ src/reports/reports.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
+
+@Injectable()
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report)
+    private reportsRepository: Repository<Report>,
+  ) {}
+}
